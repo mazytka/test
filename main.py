0e93986ab036a4cd8c3734fc12e2dd0426c1b0d3
mazytka <95392543+mazytka@users.noreply.github.com>
2022-06-19 01:37:03 +0300

    обработал меню

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 
 
 def load_level(name):
+    """Функция, принимающая имя файла уровня"""
     fullname = name
     with open(fullname, 'r') as map_file:
         level_map = []
@@ -13,6 +14,7 @@ def load_level(name):
 
 
 def draw_level(level_map):
+    """Функция отрисовки самого уровня"""
     player, x, y, box, wall = None, None, None, None, None
     for y in range(len(level_map)):
         for x in range(len(level_map[y])):
@@ -33,7 +35,7 @@ def draw_level(level_map):
 
 
 def main_menu():
-
+    """Функция отображения главного меню пользователя"""
     while True:
         window.blit(bg, (0, 0))
 
@@ -57,7 +59,7 @@ def main_menu():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if start_but.checkForInput(mouse_pos):
-                    return  False
+                    return False
 
                 if quit_but.checkForInput(mouse_pos):
                     pygame.quit()
@@ -66,66 +68,145 @@ def main_menu():
         pygame.display.update()
 
 
+def game_over():
+    while True:
+        window.fill((135, 206, 250))
+
+        mouse_pos = pygame.mouse.get_pos()
+
+        menu_title = pygame.font.SysFont("cambria", 75).render("GAME OVER", True, 'black')
+        menu_rect = menu_title.get_rect(center=(200, 50))
+
+        quit_but = Button(exit_img, 210, 300, 'EXIT')
+
+        window.blit(menu_title, menu_rect)
+
+        quit_but.changeColor(mouse_pos)
+        quit_but.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if quit_but.checkForInput(mouse_pos):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+
 def draw_sprite():
+    """Функция отрисовки главного экрана и всех спрайтов"""
     window.fill((135, 206, 250))
     all_sprites.draw(window)
+    player_group.draw(window)
+
+
+def game_run(level_txt):
+    global goals
+    player, level_x, level_y, box, wall = draw_level(load_level(level_txt))
+    run = True
+    while run:
+        clock.tick(fps)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                all_sprites.empty()
+                walls_group.empty()
+                player_group.empty()
+                boxes_group.empty()
+
+                run = False
+                game_run(level_txt)
+
+            move.update(event, player, box)
+
+        if pygame.sprite.groupcollide(boxes_group, tiles_group, True, True):
+            if not boxes_group.sprites():
+                all_sprites.empty()
+                player_group.empty()
+                walls_group.empty()
+                run = False
+
+        if pygame.sprite.spritecollideany(player, telep_sprites):
+            player.rect.x = 160
+            player.rect.y = 80
+
+        draw_sprite()
+
+        pygame.display.update()
 
 
 class Move:
-
+    """Класс для отслеживания действий персонажа с другими объектами"""
     def update(self, event, player, box):
+        global vector
         if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
             player.move_up()
+            vector = 'UP'
             if pygame.sprite.spritecollideany(player, walls_group):
                 player.move_down()
-            if pygame.sprite.collide_mask(player, box):
-                box.rect.y -= 40
+            if pygame.sprite.spritecollideany(player, boxes_group):
+                boxes_group.update(player, walls_group)
                 if pygame.sprite.spritecollideany(box, walls_group):
-                    box.rect.y += 40
+                    boxes_group.update(player, walls_group)
+                if pygame.sprite.spritecollideany(player, boxes_group):
                     player.move_down()
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
             player.move_down()
+            vector = 'DOWN'
             if pygame.sprite.spritecollideany(player, walls_group):
                 player.move_up()
-            if pygame.sprite.collide_mask(player, box):
-                box.rect.y += 40
+            if pygame.sprite.spritecollideany(player, boxes_group):
+                boxes_group.update(player, walls_group)
                 if pygame.sprite.spritecollideany(box, walls_group):
-                    box.rect.y -= 40
+                    boxes_group.update(player, walls_group)
+                if pygame.sprite.spritecollideany(player, boxes_group):
                     player.move_up()
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
             player.move_left()
+            vector = 'LEFT'
             if pygame.sprite.spritecollideany(player, walls_group):
                 player.move_right()
-            if pygame.sprite.collide_mask(player, box):
-                box.rect.x -= 40
+            if pygame.sprite.spritecollideany(player, boxes_group):
+                boxes_group.update(player, walls_group)
                 if pygame.sprite.spritecollideany(box, walls_group):
-                    box.rect.x += 40
+                    boxes_group.update(player, walls_group)
+                if pygame.sprite.spritecollideany(player, boxes_group):
                     player.move_right()
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
             player.move_right()
+            vector = 'RIGHT'
             if pygame.sprite.spritecollideany(player, walls_group):
                 player.move_left()
-            if pygame.sprite.collide_mask(player, box):
-                box.rect.x += 40
+            if pygame.sprite.spritecollideany(player, boxes_group):
+                boxes_group.update(player, walls_group)
                 if pygame.sprite.spritecollideany(box, walls_group):
-                    box.rect.x -= 40
+                    boxes_group.update(player, walls_group)
+                if pygame.sprite.spritecollideany(player, boxes_group):
                     player.move_left()
 
 
 class Tile(pygame.sprite.Sprite):
+    """Класс тайлов"""
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
         self.image = pygame.transform.scale(pygame.image.load(tile_type), (40, 40))
         self.rect = self.image.get_rect().move(40 * pos_x, 40 * pos_y)
 
-        if tile_type == 'images/wall.png':
-            self.add(all_sprites, tiles_group)
+        if tile_type == 'images/box_space.png':
+            self.add(box_spaces)
 
 
 class Teleport(pygame.sprite.Sprite):
+    """Класс телепорта"""
     def __init__(self, pos_x, pos_y):
         super().__init__()
         self.image = pygame.transform.scale(pygame.image.load('images/Teleport.png'), (40, 40))
@@ -135,6 +216,7 @@ class Teleport(pygame.sprite.Sprite):
 
 
 class Player(pygame.sprite.Sprite, Move):
+    """Класс игрока"""
     def __init__(self, pos_x, pos_y):
         super().__init__()
         self.image = pygame.transform.scale(pygame.image.load('images/player.png'), (40, 40))
@@ -146,6 +228,7 @@ class Player(pygame.sprite.Sprite, Move):
         self.y = pos_y
 
     def collide_player(self, player, telep_sprites):
+        """Проверка столкновения игрока со спрайтом телепорта"""
         if pygame.sprite.spritecollideany(player, telep_sprites):
             player.rect.x = 80
 
@@ -163,7 +246,7 @@ class Player(pygame.sprite.Sprite, Move):
 
 
 class Box(pygame.sprite.Sprite, Move):
-
+    """Класс ящика"""
     def __init__(self, pos_x, pos_y):
         super().__init__(boxes_group)
         self.image = pygame.transform.scale(pygame.image.load('images/box.png'), (40, 40))
@@ -173,8 +256,33 @@ class Box(pygame.sprite.Sprite, Move):
         self.pos_y = pos_y
         self.add(all_sprites, boxes_group)
 
+    def update(self, *args):
+        global vector
+        if vector == 'UP':
+            if pygame.sprite.collide_mask(self, args[0]):
+                self.rect.y -= 40
+            if pygame.sprite.spritecollideany(self, args[1]):
+                self.rect.y += 40
+
+        if vector == 'DOWN':
+            if pygame.sprite.collide_mask(self, args[0]):
+                self.rect.y += 40
+            if pygame.sprite.spritecollideany(self, args[1]):
+                self.rect.y -= 40
+        if vector == 'LEFT':
+            if pygame.sprite.collide_mask(self, args[0]):
+                self.rect.x -= 40
+            if pygame.sprite.spritecollideany(self, args[1]):
+                self.rect.x += 40
+        if vector == 'RIGHT':
+            if pygame.sprite.collide_mask(self, args[0]):
+                self.rect.x += 40
+            if pygame.sprite.spritecollideany(self, args[1]):
+                self.rect.x -= 40
+
 
 class Wall(pygame.sprite.Sprite):
+    """Класс стены"""
     def __init__(self, pos_x, pos_y):
         super().__init__()
         self.image = pygame.transform.scale(pygame.image.load('images/wall.png'), (40, 40))
@@ -184,6 +292,7 @@ class Wall(pygame.sprite.Sprite):
 
 
 class Button:
+    """Класс для изображения кнопок и их взаимодейсвия с курсором мыши"""
     def __init__(self, image, x_pos, y_pos, text_input):
         self.image = image
         self.x_pos = x_pos
@@ -209,6 +318,8 @@ class Button:
         else:
             self.text = main_font.render(self.text_input, True, "white")
 
+
+vector = ''
 pygame.init()
 screen_stat = {'width': 10, 'height': 10, 'tile': 40}
 fps = 30
@@ -222,81 +333,21 @@ start_img = pygame.transform.scale(pygame.image.load('images/start.png'), (136, 
 pygame.display.set_caption(title)
 
 clock = pygame.time.Clock()
-
-
-
+goals = []
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 walls_group = pygame.sprite.Group()
 boxes_group = pygame.sprite.Group()
 telep_sprites = pygame.sprite.Group()
+box_spaces = pygame.sprite.Group()
 move = Move()
-
-
-def level_1():
-    player, level_x, level_y, box, wall = draw_level(load_level('level1.txt'))
-
-    run = True
-    while run:
-        clock.tick(fps)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
-                all_sprites.empty()
-                player_group.empty()
-                run = False
-                level_1()
-
-            move.update(event, player, box)
-
-        player.collide_player(player, telep_sprites)
-
-        if pygame.sprite.spritecollideany(box, tiles_group):
-            all_sprites.empty()
-            walls_group.empty()
-            player_group.empty()
-
-            run = False
-
-        draw_sprite()
-
-        pygame.display.update()
-
-
-def level_2():
-    player, level_x, level_y, box, wall = draw_level(load_level('level2.txt'))
-    run = True
-    while run:
-        clock.tick(fps)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
-                all_sprites.empty()
-                walls_group.empty()
-                player_group.empty()
-                run = False
-                level_2()
-
-            move.update(event, player, box)
-
-        if pygame.sprite.spritecollideany(box, tiles_group):
-            run = False
-
-        draw_sprite()
-
-        pygame.display.update()
 
 
 if __name__ == '__main__':
     main_menu()
-    level_1()
-    level_2()
+    game_run('level1.txt')
+    game_run('level2.txt')
+    game_run('level3.txt')
+    game_run('level4.txt')
+    game_over()
